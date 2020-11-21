3252574c6c155f8b182ae021320d278172badfa1
tongshiwei <tongsw@mail.ustc.edu.cn>
2020-11-21 19:52:47 +0800

    [perf] remove todo

--- XCD/IRT/MXIRT/Module/module.py
+++ XCD/IRT/MXIRT/Module/module.py
@@ -281,7 +281,6 @@ class Module(module.Module):
                     )
                 )
 
-            # todo 定义模型保存方案
             if save_model:
                 if kwargs.get('prefix') and (
                         epoch % kwargs.get('save_epoch', 1) == 0 or end_epoch - 10 <= epoch <= end_epoch - 1):
